8759eaaa59a5e6f280d2c386c4b20991190c7009
Christian Rauch <Rauch.Christian@gmx.de>
2018-12-12 01:18:59 +0000

    Trimmed the branch to only the apriltag_msgs sub directory

--- node/src/apriltag2_node.cpp
+++
@@ -1,8 +0,0 @@
-#include <AprilTag2Node.hpp>
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<AprilTag2Node>());
-    rclcpp::shutdown();
-    return 0;
-}
